38d3771f6f4d7ac111dfb1d460708df15a756833
sebastien <sebastien.desalle@student.ap.be>
2025-11-17 18:21:00 +0100

    Add get plan

--- services/api-ts/src/server.ts
+++ services/api-ts/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { Pool } from "pg";
+import * as planData from "./plan.json";
 
 //database connection
 const pool = new Pool({
@@ -25,7 +26,6 @@ app.get("/api/activities", async (req: Request, res: Response) => {
     const result = await pool.query(
       "SELECT * FROM activities ORDER BY start_time DESC"
     );
-
     res.json(result.rows);
   } catch (err) {
     console.error("API: Error querying activities", err);
@@ -33,6 +33,16 @@ app.get("/api/activities", async (req: Request, res: Response) => {
   }
 });
 
+// get plan
+app.get("/api/plan", (req: Request, res: Response) => {
+  try {
+    res.json(planData.MainPlan);
+  } catch (err) {
+    console.error("API: Error reading plan.json", err);
+    res.status(500).json({ error: "internal server error" });
+  }
+});
+// start server
 app.listen(port, () => {
   console.log(`[server]: API server is running at http://localhost:${port}`);
 });
